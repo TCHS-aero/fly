fc233f8b8fd5d4c8c336267c2ac2b844333759ff
Tranquil-M <bpihwhshmh@gmail.com>
2026-02-18 19:22:08 -0800

    updated movement commands to fix issue #7

--- src/fly/core/drone.py
+++ src/fly/core/drone.py
@@ -3,6 +3,7 @@ import asyncio
 from mavsdk import System
 from mavsdk.offboard import PositionGlobalYaw, VelocityNedYaw
 
+
 class Drone:
     def __init__(self, port, *, connection_timeout=10, velocity=0.5):
         self.drone = System()
@@ -90,92 +91,100 @@ class Drone:
             north_m = ned_object.north_m
             east_m = ned_object.east_m
             down_m = ned_object.down_m
-            return north_m, east_m, down_m
+            return (north_m, east_m, down_m)
 
     async def stop_movement(self):
         north, east, down = await self.current_ned()
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
         await asyncio.sleep(1)
-        await self.drone.offboard.set_velocity_ned(
-            VelocityNedYaw(0.0, 0, 0.0, 0)
-        )
+        await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0, 0.0, 0))
         await asyncio.sleep(1)
         await self.drone.offboard.stop()
 
-    async def move_left_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_left_offset(self, velocity, distance, *, yaw=0):
+        _, east, _ = await self.current_ned()
+        end_distance = east - distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
-        await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(0.0, velocity * -1, 0.0, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_distance <= east:
+            _, east, _ = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_right_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_right_offset(self, velocity, distance, *, yaw=0):
+        _, east, _ = await self.current_ned()
+        end_point = east + distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
-        await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(0.0, velocity, 0.0, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_point >= east:
+            _, east, _ = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_down_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_down_offset(self, velocity, distance, *, yaw=0):
+        _, _, down = await self.current_ned()
+        end_point = down + distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
         await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(0.0, 0.0, velocity, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_point >= down:
+            _, _, down = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_up_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_up_offset(self, velocity, distance, *, yaw=0):
+        _, _, down = await self.current_ned()
+        end_point = down - distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
         await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(0.0, 0.0, velocity * -1, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_point <= down:
+            _, _, down = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_forward_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_forward_offset(self, velocity, distance, *, yaw=0):
+        north, _, _ = await self.current_ned()
+        end_point = north + distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
         await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(velocity, 0.0, 0.0, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_point >= north:
+            north, _, _ = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_backward_offset(self, velocity, *, yaw = 0, seconds = 5):
-        north, east, down = await self.current_ned()
+    async def move_backward_offset(self, velocity, distance, *, yaw=0):
+        north, _, _ = await self.current_ned()
+        end_point = north - distance
         await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
         await self.drone.offboard.start()
         await asyncio.sleep(1)
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(velocity * -1, 0.0, 0.0, yaw)
         )
-
-        await asyncio.sleep(seconds)
+        while end_point <= north:
+            north, _, _ = await self.current_ned()
+            await asyncio.sleep(0.2)
         await self.drone.offboard.stop()
 
-    async def move_to_waypoint(self, dict, yaw = 0.0):
+    async def move_to_waypoint(self, dict, yaw=0.0):
         lat, lon, alt = dict.values()
         print(lat, lon, alt, yaw, self.mode)
         clat, clon, calt = await self.current_position()
--- src/fly/interfaces/gui.py
+++ src/fly/interfaces/gui.py
@@ -4,11 +4,22 @@ import re
 
 from fly.core.drone import Drone
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QPushButton, QLabel,QDoubleSpinBox,
-    QVBoxLayout, QWidget, QTextEdit, QLineEdit, QTabWidget, QGridLayout,QSizePolicy
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+    QDoubleSpinBox,
+    QVBoxLayout,
+    QWidget,
+    QTextEdit,
+    QLineEdit,
+    QTabWidget,
+    QGridLayout,
+    QSizePolicy,
 )
-from PyQt6.QtGui import QFont, QFontDatabase, QIcon
+from PyQt6.QtGui import QFontDatabase, QIcon
 from qasync import asyncSlot, QEventLoop
+
 
 def is_valid_port(port: str) -> bool:
     udp_pattern = r"^udp(?:in|out)?://([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}$"
@@ -33,11 +44,12 @@ class TC_Drone_App(QMainWindow):
         central = QWidget()
         main_layout = QVBoxLayout()
 
-        
         self.tabs = QTabWidget()
 
-        #implementing font
-        self.font_id = QFontDatabase.addApplicationFont("src/fly/assets/BlackOpsOne-Regular.ttf")
+        # implementing font
+        self.font_id = QFontDatabase.addApplicationFont(
+            "src/fly/assets/BlackOpsOne-Regular.ttf"
+        )
         if self.font_id != -1:  # Success check
             font_families = QFontDatabase.applicationFontFamilies(self.font_id)
             custom_font_name = font_families[0]  # Usually index 0
@@ -47,7 +59,7 @@ class TC_Drone_App(QMainWindow):
 
         general_widget = QWidget()
         general_layout = QVBoxLayout()
-        
+
         self.port_edit = QLineEdit()
         self.port_edit.setPlaceholderText("Port, e.g. udpin://0.0.0.0:14540")
 
@@ -55,7 +67,9 @@ class TC_Drone_App(QMainWindow):
         self.logo.setStyleSheet(f"font-size:40px; font-family: {custom_font_name};")
         self.status = QLabel("Status: Disconnected")
         self.console = QTextEdit()
-        self.console.setStyleSheet("background-color: black")
+        self.console.setStyleSheet("""background-color: black;
+        color: white;
+        """)
         self.console.setReadOnly(True)
 
         self.button_connect = QPushButton("1. Connect to the drone")
@@ -89,19 +103,17 @@ class TC_Drone_App(QMainWindow):
         general_widget.setLayout(general_layout)
         self.tabs.addTab(general_widget, "General")
 
-
         movement_widget = QWidget()
         movement_layout = QVBoxLayout()
-        
+
         self.movement_controls_text = QLabel("Movement Controls")
-        
+
         self.logo = QLabel("TCHS Aero GUI ver.1")
         self.logo.setStyleSheet("background-color: red")
-        self.logo.setGeometry(400,100,500,50)
-
+        self.logo.setGeometry(400, 100, 500, 50)
 
         grid = QGridLayout()
-        #buttons for drone movement
+        # buttons for drone movement
         self.button_up = QPushButton("Up")
         self.button_up.clicked.connect(self.move_up)
         self.button_down = QPushButton("Down")
@@ -129,13 +141,12 @@ class TC_Drone_App(QMainWindow):
             QPushButton:!checked { background-color: darkgreen; color: white; }
         """)
 
-
-        #dynamic button sizes acccording to the user's expansion of the window
+        # dynamic button sizes acccording to the user's expansion of the window
         policy_connect = self.button_connect.sizePolicy()
         policy_connect.setVerticalPolicy(QSizePolicy.Policy.Expanding)
         self.button_connect.setSizePolicy(policy_connect)
         self.button_connect.setMinimumHeight(60)
-    
+
         policy_takeoff = self.button_takeoff.sizePolicy()
         policy_takeoff.setVerticalPolicy(QSizePolicy.Policy.Expanding)
         self.button_takeoff.setSizePolicy(policy_takeoff)
@@ -146,8 +157,6 @@ class TC_Drone_App(QMainWindow):
         self.button_land.setSizePolicy(policy_land)
         self.button_land.setMinimumHeight(60)
 
-
-
         policy_stop_mvt = self.button_stop_movement.sizePolicy()
         policy_stop_mvt.setVerticalPolicy(QSizePolicy.Policy.Expanding)
         self.button_stop_movement.setSizePolicy(policy_stop_mvt)
@@ -157,7 +166,6 @@ class TC_Drone_App(QMainWindow):
         policy_rth.setVerticalPolicy(QSizePolicy.Policy.Expanding)
         self.button_rth.setSizePolicy(policy_rth)
         self.button_rth.setMinimumHeight(60)
-
 
         policy_up = self.button_up.sizePolicy()
         policy_up.setVerticalPolicy(QSizePolicy.Policy.Expanding)
@@ -189,7 +197,6 @@ class TC_Drone_App(QMainWindow):
         self.button_backward.setSizePolicy(policy_backward)
         self.button_backward.setMinimumHeight(60)
 
-
         grid.addWidget(self.button_up, 0, 4)
         grid.addWidget(self.button_down, 2, 4)
         grid.addWidget(self.button_forward, 0, 1)
@@ -198,9 +205,11 @@ class TC_Drone_App(QMainWindow):
         grid.addWidget(self.button_right, 1, 2)
         grid.addWidget(self.button_stop_movement, 1, 1)
         grid.addWidget(self.button_rth, 1, 4)
-        
+
         self.velocity_text = QLabel("Velocity")
         self.velocity = QDoubleSpinBox()
+        self.distance_text = QLabel("Distance")
+        self.distance = QDoubleSpinBox()
         self.yaw_text = QLabel("Yaw")
         self.yaw = QDoubleSpinBox()
 
@@ -211,6 +220,8 @@ class TC_Drone_App(QMainWindow):
         movement_layout.addWidget(self.velocity)
         movement_layout.addWidget(self.yaw_text)
         movement_layout.addWidget(self.yaw)
+        movement_layout.addWidget(self.distance_text)
+        movement_layout.addWidget(self.distance)
         movement_widget.setLayout(movement_layout)
         self.tabs.addTab(movement_widget, "Movement")
 
@@ -235,11 +246,13 @@ class TC_Drone_App(QMainWindow):
 
             # Validate port string
             if not is_valid_port(port):
-                self.log("-- Invalid port format. Please specify a valid UDP, TCP, or Serial port.")
+                self.log(
+                    "-- Invalid port format. Please specify a valid UDP, TCP, or Serial port."
+                )
                 self.status.setText("Status: Invalid port")
                 self.button_connect.setChecked(False)
                 return
-            
+
             self.drone = Drone(port=port)
 
             self.log("Connecting...")
@@ -248,14 +261,16 @@ class TC_Drone_App(QMainWindow):
                 if connected:
                     self.status.setText("Status: Connected")
                     self.log("-- Connected")
-                    self.button_takeoff.setEnabled(True) # Enable Takeoff
-                    
+                    self.button_takeoff.setEnabled(True)  # Enable Takeoff
+
                     # Optional: Get position confirmation
                     lat, lon, alt = await self.drone.current_position()
                     self.log(f"Pos: {lat:.5f}, {lon:.5f}, {alt:.1f}m")
                 else:
                     self.status.setText("Status: Connection Failed")
-                    self.log("-- Connection test failed; consider trying a different port.")
+                    self.log(
+                        "-- Connection test failed; consider trying a different port."
+                    )
                     self.button_connect.setChecked(False)
             except Exception as e:
                 self.log(f"Error: {e}")
@@ -272,7 +287,7 @@ class TC_Drone_App(QMainWindow):
         self.log("Starting Takeoff Sequence...")
         self.button_takeoff.setEnabled(False)
         try:
-            await self.drone.takeoff(10.0) 
+            await self.drone.takeoff(10.0)
             self.log("Takeoff...")
             self.status.setText("Status: Taken off")
             self.button_land.setEnabled(True)
@@ -284,7 +299,7 @@ class TC_Drone_App(QMainWindow):
     async def on_land(self):
         self.log("Starting Landing Sequence...")
         try:
-            await self.drone.land() 
+            await self.drone.land()
             self.log("Landing...")
             self.status.setText("Status: Landing")
             self.button_land.setChecked(False)  # Reset toggle
@@ -300,92 +315,170 @@ class TC_Drone_App(QMainWindow):
         self.log("Starting Moving Up Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_up_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Up...")
             self.status.setText("Status: Moving Up")
-
+            await self.drone.move_up_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
             self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def move_down(self):
         self.log("Starting Moving Down Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_down_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Down...")
             self.status.setText("Status: Moving Down")
-
+            await self.drone.move_down_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
-            self.log(f"Moving Down Error: {e}")
+            self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def move_left(self):
         self.log("Starting Moving Left Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_left_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Left...")
             self.status.setText("Status: Moving Left")
-
+            await self.drone.move_left_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
-            self.log(f"Moving Left Error: {e}")
+            self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def move_right(self):
         self.log("Starting Moving Right Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_right_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Right...")
             self.status.setText("Status: Moving Right")
-
+            await self.drone.move_right_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
-            self.log(f"Moving Right Error: {e}")
+            self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def move_forward(self):
         self.log("Starting Moving Forward Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_forward_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Forward...")
             self.status.setText("Status: Moving Forward")
-
+            await self.drone.move_forward_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
-            self.log(f"Moving Forward Error: {e}")
+            self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def move_backward(self):
         self.log("Starting Moving Backward Sequence...")
         self.velocity_flt = self.velocity.value()
         self.yaw_flt = self.yaw.value()
-        print(self.velocity_flt, self.yaw_flt)
+        self.distance_flt = self.distance.value()
+
+        if self.distance_flt < 0 or self.velocity_flt < 0:
+            self.log("All values must be greater than 0.")
+            return
+        elif not self.distance_flt or not self.velocity_flt:
+            self.log("Distance and Velocity are required to execute this command.")
+            return
+
+        print(self.velocity_flt, self.yaw_flt, self.distance_flt)
         try:
-            await self.drone.move_backward_offset(velocity=self.velocity_flt, yaw=self.yaw_flt)
             self.log("Moving Backward...")
             self.status.setText("Status: Moving Backward")
-
+            await self.drone.move_backward_offset(
+                distance=self.distance_flt, velocity=self.velocity_flt, yaw=self.yaw_flt
+            )
+            self.log("Movement finished")
+            self.status.setText("Status: Movement stopped.")
         except Exception as e:
-            self.log(f"Moving Backward Error: {e}")
+            self.log(f"Moving Up Error: {e}")
+            self.log("Are you connected?")
 
     @asyncSlot()
     async def stoping_movement(self):
         self.log("Stoping Movement...")
         try:
-            await self.drone.stop_movement()
             self.log("Stoping Movement...")
             self.status.setText("Status: Stoping Movement")
+            await self.drone.stop_movement()
 
         except Exception as e:
             self.log(f"Stoping Movement Error: {e}")
@@ -400,20 +493,19 @@ class TC_Drone_App(QMainWindow):
 
         except Exception as e:
             self.log(f"Return to Launch Error: {e}")
-    
-
 
 
 def main():
     app = QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
-    
+
     win = TC_Drone_App()
     win.show()
-    
+
     with loop:
         loop.run_forever()
+
 
 if __name__ == "__main__":
     main()
