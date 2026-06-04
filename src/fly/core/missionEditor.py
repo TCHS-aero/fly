c19ce741a15a6aa5971e67fbf9ff8750739b360e
round-panda <jb271336@gmail.com>
2026-06-04 13:14:53 -0700

    introduce require_safe_edit_window() decorator to utilize seconds_until_next_waypoint

--- src/fly/core/missionEditor.py
+++ src/fly/core/missionEditor.py
@@ -1,9 +1,21 @@
 import asyncio
 import json
 from pathlib import Path
+from time import time
 
 from fly.core.mission import Mission
 
+import functools
+def require_safe_edit_window(func):
+    # checks if it's safe to edit. decorator so code isn't duplicated
+    @functools.wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        if not await self.is_safe_to_edit():
+            print("Cannot edit mission now; not enough time before next waypoint")
+            return
+        # if safe, do original function
+        return await func(self, *args, **kwargs)
+    return wrapper # afaik this runs once for each decorator call during Module Import Time
 
 class MissionEditor:
     """
@@ -23,12 +35,29 @@ class MissionEditor:
 
     async def current_index(self) -> int:
         # returns current waypoint index from mavsdk, or -1 if not active
+        pass
 
     async def seconds_until_next_waypoint(self) -> float:
         # returns estimated seconds until the next waypoint
         # straight-line distance / ground speed. returns inf if speed = 0
         # The GUI uses this to enable the edit button when the value exceeds 8s
+        # 1s to detect safe window, 1s to call pause_mission(), 3s drone to stop, 2s to upload, 1s buffer
+        pass
 
+    async def is_safe_to_edit(self, time_buffer_s: int = 8) -> bool:
+        if await self.current_index() < 0:
+            # mission not active so we can't insert/append anything
+            return False
+        time_to_next = await self.seconds_until_next_waypoint()
+
+        if time_to_next > time_buffer_s:
+            print(f"SAFE: {time_to_next:.1f}s until next waypoint")
+            return True
+        else:
+            print(f"UNSAFE: {time_to_next:.1f}s until next waypoint")
+            return False
+
+    @require_safe_edit_window
     async def append_waypoint(self, wp:dict):
         # appends waypoint to end of mission
         async with self._lock:
@@ -39,11 +68,18 @@ class MissionEditor:
             self.save_waypoints(wps)
             await self._upload_and_resume(wps,idx)
 
+        # ...
+
+    @require_safe_edit_window
     async def insert_waypoint(self, at: int, wp: dict):
         # insert a waypoint at position 'at'
+        pass
 
+    @require_safe_edit_window
     async def remove_waypoint(self, at: int):
-        # removes waypoint at position 'at'
+        if not await self.is_safe_to_edit():
+            print("Cannot edit mission now.")
+            return
 
     # private helpers
 
@@ -52,6 +88,7 @@ class MissionEditor:
 
     async def _upload_and_resume(self, waypoints: list[dict], resume_index:int):
         # converts waypoints to missionitems, uploads, sets item, starts mission
+        pass
 
     # JSON I/O
     def load_waypoints(self) -> list[dict]:
